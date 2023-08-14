e3bf30d15592571e5e467f62eb77d58d1b7cdfc5
MiladinKi <miladinkovacevic12@gmail.com>
2023-08-14 09:54:14 +0200

    Primer1
    Napisan Primer1 za promenljive

---
+++ src/promenljive/Primer1.java
@@ -0,0 +1,23 @@
+package promenljive;
+
+public class Primer1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 3;
+		int b;
+		System.out.println("Vrednost promenljive a je:");
+		System.out.println(a);
+		b = a + 12;
+		System.out.println("Vrednost promenljive b=a+12 je:");
+		System.out.println(b);
+		a = a + 1;
+		System.out.println("Vrednost promenljive a=a+1 je:");
+		System.out.println(a);
+		b = b + 100;
+		System.out.println("Vrednost promenljive b=b+100 je:");
+		System.out.println(b);
+
+	}
+
+}
